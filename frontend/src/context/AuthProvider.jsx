81b5a024b3c1dd7a08c92e155903a81504e846d2
Gulistane Raza <gulistaneraza01@gmail.com>
2025-03-25 01:47:36 +0530

    incompleted reset password

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -51,7 +51,6 @@ function AuthProvider({ children }) {
 
   const login = async (credential) => {
     dispatch({ type: "AUTH_LOADING" });
-
     try {
       const res = await axios.post(apiRoutes.loginUrl, credential, {
         withCredentials: true,
